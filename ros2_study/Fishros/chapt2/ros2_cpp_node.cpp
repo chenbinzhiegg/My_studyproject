b9eb77f90011bce8b81567497c378c912a15b286
chenbinzhiegg <chenzhibin2099@hotmail.com>
2026-05-17 17:07:26 +0800

    study

---
+++ ros2_study/Fishros/chapt2/ros2_cpp_node.cpp
@@ -0,0 +1,15 @@
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc,char** argv)
+{
+    rclcpp::init(argc,argv);
+    auto node = std::make_shared<rclcpp::Node>("cpp_node");
+    RCLCPP_INFO(node->get_logger(),"你好c++节点！");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
+
+// std::cout<<"参数数量"<<argc<<std::endl;
+// std::cout<<"程序名字"<<argv[0]<<std::endl;
---
+++ ros2_study/Fishros/chapt2/ros2_python_node.py
@@ -0,0 +1,13 @@
+import rclpy
+from rclpy.node import Node
+
+def main():
+    rclpy.init()
+    node = Node('python_node')  #node名字
+    node.get_logger().info("你好,python 节点")
+    node.get_logger().warn("你好,python 节点")
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__=='__main__':
+    main()
